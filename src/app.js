23251b415ca09536897ba5f8d56599a5b4c2d0e9
Mohit kumar <itsmohit3005@gmail.com>
2025-01-27 20:58:18 +0530

    fix: update allowed origins for CORS in app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,8 @@ app.use(
     origin: [
       "https://resturant-reservatation.onrender.com",
       "https://resturant-reservatation-system.onrender.com",
-      "https://swiftgate.online", // Add your service domain here
+      "https://resturant-reservatation-system-frontend.onrender.com",
+      "https://resturant-reservatation-system-1.onrender.com",
       "http://localhost:3000",
     ],
     credentials: true, // Allow credentials (cookies)
