c69e29a122630daec1a254f077a5f413f8cf6dd8
Mohit kumar <itsmohit3005@gmail.com>
2025-03-05 23:14:36 +0530

    feat: enhance database configuration with additional environment variables

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,74 @@
 require("dotenv").config();
 
 const path = require("path");
-const { DATABASE_URL, DATABASE_APITABLE_URL } = process.env;
+const {
+  DATABASE_URL,
+  USER_NAME,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+  DEBUG,
+  DATABASE_APITABLE_URL,
+} = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    pool: { min: 1, max: 5 },
+    connection: {
+      host: DATABASE_URL,
+      port: 5432,
+      user: USER_NAME,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false, // Skip certificate validation
+      },
+    },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    debug: false,
   },
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    pool: { min: 1, max: 5 },
+    connection: {
+      host: DATABASE_URL,
+      port: 5432,
+      user: USER_NAME,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false, // Skip certificate validation
+      },
+    },
+
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    debug: false,
   },
   developmentApi: {
     client: "postgresql",
-    connection: DATABASE_APITABLE_URL,
+    connection: {
+      host: DATABASE_APITABLE_URL,
+      port: 5432,
+      user: USER_NAME,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+      ssl: {
+        rejectUnauthorized: false, // Skip certificate validation
+      },
+    },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    debug: false,
   },
 };
